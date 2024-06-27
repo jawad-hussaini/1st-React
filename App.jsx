4fd867d51b0ea3582030365841fdec386fd076ae
Syed Jawad <jawadhussaini1804@gmail.com>
2024-06-27 18:34:40 +0500

    1st react practice

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Login from './Register/login'
+import {Font} from './Register/font'
+function App() {
+  return (
+    <>
+    <Login/>
+    <Font/>
+    </>
+  )
+}
+
+export default App
---
+++ Register/login.jsx
@@ -0,0 +1,12 @@
+
+
+export default function Login() {
+
+
+
+  return (
+    <div>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error laboriosam rerum, doloribus, animi magni laborum numquam iusto consequatur a voluptas amet repellat! Laudantium repudiandae sapiente, animi quas quae sunt accusantium?</p>
+    </div>
+  )
+}
